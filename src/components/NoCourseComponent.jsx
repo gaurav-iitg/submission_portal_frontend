4fc6f69421927bf410493999f6aca0f72bd52315
gauravkumar5089 <gauravkumar5089@gmail.com>
2023-04-27 17:21:09 +0530

    NoCourseComponent successfully added on home

--- src/components/NoCourseComponent.jsx
+++ src/components/NoCourseComponent.jsx
@@ -3,7 +3,7 @@ import logo from "../assests/logo.png";
 
 function NoCourseComponent() {
   return (
-    <div className="h-screen w-screen bg-gray-100 flex justify-center items-center">
+    <div className="h-full w-screen bg-gray-100 flex justify-center items-center">
       <img src={logo} alt="Watermark" className="opacity-25 absolute" />
     </div>
   );
