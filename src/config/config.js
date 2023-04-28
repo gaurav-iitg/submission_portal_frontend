837cf0a979dbd90575ac39e71f444dde189b94ae
Anurag Ravi <kumarayush1014@gmail.com>
2023-04-29 00:20:43 +0530

    added backend url

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 export const config = {
     // backendUrl: 'http://localhost:5000',
-    backendUrl: 'http://172.16.114.3:5000'
+    // backendUrl: 'http://172.16.114.3:5000',
+    backendUrl: 'https://submission-portal.onrender.com',
 };
