2d07f812d985e91917c1fb68c1b7daec763a3dfc
Anurag Ravi <kumarayush1014@gmail.com>
2023-09-02 14:51:57 +0530

    changed backend url

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 export const config = {
     // backendUrl: 'http://localhost:5000',
     // backendUrl: 'http://172.16.114.3:5000',
-    backendUrl: 'https://submission-portal.onrender.com',
+    // backendUrl: 'https://submission-portal.onrender.com',
+    backendUrl: 'https://submission-portal-m9n2.onrender.com',
 };
